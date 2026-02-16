4692efb2ddb348db2ba1f4eb0b7604a3fbe986cf
ayush <ayushdhewale@gmail.com>
2026-02-16 23:53:14 +0530

    Auto migrate for Vercel

--- FormHandling/wsgi.py
+++ FormHandling/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for FormHandling project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FormHandling.settings')
+
+# Run migrations automatically on startup
+try:
+    call_command('migrate', interactive=False)
+except:
+    pass
 
 app = get_wsgi_application()
