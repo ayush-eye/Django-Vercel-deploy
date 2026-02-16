f7ee324e546d40895914d2289f558c35713e1243
ayush <ayushdhewale@gmail.com>
2026-02-16 23:32:47 +0530

    Django Vercel deploy

--- FormHandling/settings.py
+++ FormHandling/settings.py
@@ -80,10 +80,9 @@ WSGI_APPLICATION = 'FormHandling.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/tmp/db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
